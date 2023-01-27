53eb276616f97bcfb2e4ea2c1a19bee1e6183899
RafaelAlexandre007 <rafael.alexandre013@gmail.com>
2023-01-27 19:40:07 -0300

    Método GET

--- my-bank-app/index.js
+++ my-bank-app/index.js
@@ -4,6 +4,7 @@ import { promises as fs } from "fs";
 
 const { readFile, writeFile } = fs;
 
+global.fileName = "accounts.json";
 
 const app = express();
 app.use(express.json());
@@ -14,7 +15,7 @@ app.use("/account", accountRouter);
 app.listen(3000, async () => {
 
     try {
-        await readFile("accounts.json");
+        await readFile(global.fileName);
         console.log("API Started!");
     } catch (error) {
         const initialJson = {
@@ -22,7 +23,7 @@ app.listen(3000, async () => {
             accounts: []
         }
 
-        writeFile("accounts.json", JSON.stringify(initialJson)).then(() => {
+        writeFile(global.fileName, JSON.stringify(initialJson)).then(() => {
             console.log("API Started and File Create");
 
         }).catch(error => {
--- my-bank-app/routes/accounts.js
+++ my-bank-app/routes/accounts.js
@@ -9,17 +9,27 @@ router.post("/", async (req, res) => {
 
     try {
         let account = req.body;
-        const data = JSON.parse(await readFile("accounts.json"));
+        const data = JSON.parse(await readFile(global.fileName));
 
         account = { id: data.nextId++, ...account };
         data.accounts.push(account);
 
-        await writeFile("accounts.json", JSON.stringify(data));
+        await writeFile(global.fileName, JSON.stringify(data));
 
         res.send(account);
 
     } catch (err) {
         res.status(400).send({ error: err.message })
+    }
+})
+
+router.get("/", async (req, res) => {
+    try {
+        const data = JSON.parse(await readFile(global.fileName));
+        delete data.nextId;
+        res.send(data);
+    } catch (err) {
+        res.status(400).send({ error: err.message });
     }
 })
 
